b15a14e08ad018932671ea2475d59e1738cee642
Yaroslav Mytkalyk <docd1990@gmail.com>
2018-04-25 12:32:32 +0300

    MutableSettingsRepository: reorder functions alphabetically

--- app/src/main/java/com/doctoror/particleswallpaper/domain/repository/MutableSettingsRepository.kt
+++ app/src/main/java/com/doctoror/particleswallpaper/domain/repository/MutableSettingsRepository.kt
@@ -18,19 +18,17 @@ package com.doctoror.particleswallpaper.domain.repository
 import android.support.annotation.ColorInt
 
 /**
- * Created by Yaroslav Mytkalyk on 31.05.17.
- *
  * [SettingsRepository] with mutators for settings
  */
-interface MutableSettingsRepository: SettingsRepository {
+interface MutableSettingsRepository : SettingsRepository {
 
-    fun setNumDots(numDots: Int)
-    fun setFrameDelay(frameDelay: Int)
-    fun setStepMultiplier(stepMultiplier: Float)
-    fun setDotScale(dotScale: Float)
-    fun setLineScale(lineScale: Float)
-    fun setLineDistance(lineDistance: Float)
-    fun setParticlesColor(@ColorInt color: Int)
-    fun setBackgroundUri(uri: String)
     fun setBackgroundColor(@ColorInt color: Int)
+    fun setBackgroundUri(uri: String)
+    fun setDotScale(dotScale: Float)
+    fun setFrameDelay(frameDelay: Int)
+    fun setLineDistance(lineDistance: Float)
+    fun setLineScale(lineScale: Float)
+    fun setNumDots(numDots: Int)
+    fun setParticlesColor(@ColorInt color: Int)
+    fun setStepMultiplier(stepMultiplier: Float)
 }
