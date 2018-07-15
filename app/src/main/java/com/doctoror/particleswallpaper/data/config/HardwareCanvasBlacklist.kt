209434dd2803345d75cd56c51e4ea0257e22a532
Yaroslav Mytkalyk <docd1990@gmail.com>
2018-07-15 15:39:53 +0300

    Add devices to hardware canvas blacklist

--- app/src/main/java/com/doctoror/particleswallpaper/data/config/HardwareCanvasBlacklist.kt
+++ app/src/main/java/com/doctoror/particleswallpaper/data/config/HardwareCanvasBlacklist.kt
@@ -4,14 +4,28 @@ object HardwareCanvasBlacklist {
 
     private val blackList = hashSetOf(
             "bullhead",
+            "degaswifiue",
             "dreamlte",
             "gemini",
+            "grandneove3g",
+            "heroqlteaio",
             "HWANE",
             "HWCMR09",
+            "HWSTF",
             "joan",
+            "lithium",
+            "nash",
+            "OnePlus6",
+            "P450L10",
             "polaris",
+            "sagit",
+            "scorpio",
+            "taimen",
+            "TECNO-CA7",
+            "vince",
             "wayne",
-            "whyred"
+            "whyred",
+            "ysl"
     )
 
     fun isNotBlacklistedForLockHardwareCanvas(device: String) = !blackList.contains(device)
