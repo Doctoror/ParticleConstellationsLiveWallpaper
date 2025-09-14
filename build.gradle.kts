e76be7261996603e652ef605d0da3a6ab655c121
Yaroslav Mytkalyk <docd1990@gmail.com>
2025-09-14 13:02:33 +0200

    Define plugins in version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version libs.versions.agp apply false
-    id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
